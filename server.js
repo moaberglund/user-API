d84811171dcce66c209e9610296a98a5951d64b4
moaberglund <moa.21@hotmail.com>
2025-02-19 13:55:42 +0100

    clean server, install bcrypt

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
