de8201c13d69fee8c85253ebe203c1448c43ec08
moaberglund <moa.21@hotmail.com>
2025-02-19 13:44:04 +0100

    get user profile

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -45,3 +45,16 @@ exports.login = async (req, res) => {
         res.status(500).json({message: err.message});
     }
 };
+
+// Get user profile (protected route)
+exports.getProfile = async (req, res) => {
+    try {
+        const user = await User.findById(req.user.userId).select("-password"); // exclude password
+        if(!user) {
+            return res.status(404).json({message: "User not found!"});
+        }
+        res.json(user);
+    } catch (err) {
+        res.status(500).json({message: err.message});
+    }
+};
