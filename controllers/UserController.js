b0b7b1de5aec5b095ca45f17a90742f4da74b2d5
moaberglund <moa.21@hotmail.com>
2025-02-19 13:40:00 +0100

    login user

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -20,3 +20,28 @@ exports.register = async (req, res) => {
         res.status(500).json({ message: err.message });
     }
 };
+
+// Login a user
+exports.login = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+
+        // Check if the user exists
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(401).json({ message: "Invalid username or password." });
+        }
+
+        // Compare password
+        const isMatch = await user.comparePassword(password);
+        if (!isMatch) {
+            return res.status(401).json({ message: "Invalid username or password." });
+        }
+
+        // Create JWT token
+        const token = jwt.sign({ userId: user._id}, process.env.JWT_SECRET_KEY, {expiresIn: "12h"});
+        res.json({ token });
+    } catch (err) {
+        res.status(500).json({message: err.message});
+    }
+};
