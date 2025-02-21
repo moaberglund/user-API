f487a66c3ec2508da06d31f5d7230eb89e4db746
moaberglund <moa.21@hotmail.com>
2025-02-21 16:30:44 +0100

    test to remove validate route (debug)

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const auth = require('../middleware/auth');
 
 // Import User Controller
-const { register, login, getProfile, updateProfile, deleteProfile, validateToken } = require('../controllers/UserController');
+const { register, login, getProfile, updateProfile, deleteProfile } = require('../controllers/UserController');
 
 // Routes
 router.post('/register', register);
@@ -11,6 +11,5 @@ router.post('/login', login);
 router.get('/profile', auth, getProfile);
 router.put('/profile', auth, updateProfile);
 router.delete('/profile', auth, deleteProfile)
-router.get('/validate', auth, validateToken);
 
 module.exports = router;
